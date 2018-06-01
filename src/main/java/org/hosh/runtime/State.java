d512e7ff75d60a541838b7cb1c4a3244620d258a
Davide Angelocola <davide.angelocola@gmail.com>
2018-06-01 23:17:27 +0200

    dev: tentative redux state

---
+++ src/main/java/org/hosh/runtime/State.java
@@ -0,0 +1,32 @@
+package org.hosh.runtime;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hosh.spi.Command;
+
+/**
+ * Internal state of the shell.
+ * 
+ * - can be read freely
+ * - can be modified only via controlled side-effecting actions
+ *
+ * The state can be passed freely to components:
+ * - modules (e.g. help module) can use to produce some documentation or to change current working directory 
+ * - completer can use it to help the user to autocomplete commands
+ * - to distribute read-only configuration to every component
+ */
+public class State {
+
+	// version of hosh, cannot be change
+	private final String version = "1.0";
+	
+	// current working directory
+	private final Path cwd = Paths.get(".");
+	
+	private final Map<String, Command> commands = new HashMap<>();
+	private final Map<String, Module> modules = new HashMap<>();
+	
+}
