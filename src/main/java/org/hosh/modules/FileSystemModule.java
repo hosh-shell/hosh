eef09af4e487f00ffc3493e5e03ffcf0658c1947
Davide Angelocola <davide.angelocola@gmail.com>
2018-06-17 00:14:19 +0200

    dev: cat command

--- src/main/java/org/hosh/modules/FileSystemModule.java
+++ src/main/java/org/hosh/modules/FileSystemModule.java
@@ -1,11 +1,14 @@
 package org.hosh.modules;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.hosh.spi.Channel;
 import org.hosh.spi.Command;
@@ -23,6 +26,7 @@ public class FileSystemModule implements Module {
 		commandRegistry.registerCommand("cd", new ChangeDirectory());
 		commandRegistry.registerCommand("ls", new ListFiles());
 		commandRegistry.registerCommand("cwd", new CurrentWorkingDirectory());
+		commandRegistry.registerCommand("cat", new Cat());
 	}
 
 	public static class ListFiles implements Command, StateAware {
@@ -98,6 +102,44 @@ public class FileSystemModule implements Module {
 				default:
 					err.send(Record.of("error", Values.ofText("expecting one path argument")));
 					break;
+			}
+		}
+
+	}
+
+	public static class Cat implements Command, StateAware {
+
+		private State state;
+
+		@Override
+		public void setState(State state) {
+			this.state = state;
+		}
+
+		@Override
+		public void run(List<String> args, Channel out, Channel err) {
+			switch (args.size()) {
+				case 1:
+					Path path = Paths.get(state.getCwd().toString(), args.get(0));
+					if (Files.isReadable(path)) {
+						readLines(out, path);
+					} else {
+						err.send(Record.of("error", Values.ofText("not readable file")));
+					}
+					break;
+				default:
+					err.send(Record.of("error", Values.ofText("expecting one path argument")));
+					break;
+			}
+		}
+
+		private void readLines(Channel out, Path path) {
+			try (Stream<String> lines = Files.lines(path, StandardCharsets.UTF_8)) {
+				lines.forEach(line -> {
+					out.send(Record.of("line", Values.ofText(line)));
+				});
+			} catch (IOException e) {
+				throw new UncheckedIOException(e);
 			}
 		}
 
