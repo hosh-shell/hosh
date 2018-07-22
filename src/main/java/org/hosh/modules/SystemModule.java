ae0ec163ac396a8c8c5b54e88ed82643c9797d70
Davide Angelocola <davide.angelocola@gmail.com>
2018-07-22 18:21:40 +0200

    ref: cleanups

--- src/main/java/org/hosh/modules/SystemModule.java
+++ src/main/java/org/hosh/modules/SystemModule.java
@@ -2,7 +2,6 @@ package org.hosh.modules;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import org.hosh.doc.Todo;
--- src/main/java/org/hosh/spi/Channel.java
+++ src/main/java/org/hosh/spi/Channel.java
@@ -1,10 +1,7 @@
 package org.hosh.spi;
 
 /**
- * Handling of output of records
- * 
- * TOOD: by now it is only about interactive shell, it could be extended to
- * handle an out-of-band record with column names or printing JSON to a file.
+ * Handling of output of records.
  */
 public interface Channel {
 
