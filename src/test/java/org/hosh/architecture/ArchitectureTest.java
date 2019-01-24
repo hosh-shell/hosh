472bc58930983e7b835dc7a4c8b48d0b361dc2c8
Davide Angelocola <davide.angelocola@gmail.com>
2019-01-24 22:33:14 +0100

    simplifying architecture test
    enforcing proper usage of spi from modules (e.g. it is not possible
    to use implementation detail (runtime package) inside modules.

--- src/test/java/org/hosh/architecture/ArchitectureTest.java
+++ src/test/java/org/hosh/architecture/ArchitectureTest.java
@@ -11,12 +11,10 @@ import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.core.importer.ImportOption;
 
 /**
- * Enforcing package dependencies of the project:
- * <ul>
- * <li>modules can see only spi</li>
- * <li>runtime can see spi but spi cannot see runtime</li>
- * </ul>
- * The short term goal is to prepare ground for modules.
+ * Enforcing proper package dependencies of the project (e.g. modules can see
+ * only spi.
+ *
+ * The long term goal is to prepare ground for jmod/jlink.
  */
 public class ArchitectureTest {
 	@Test
@@ -30,9 +28,6 @@ public class ArchitectureTest {
 				.should().accessClassesThat().resideInAnyPackage("..spi..", "java..")
 				.check(importedClasses);
 		noClasses().that().resideInAPackage("..modules..")
-				.should().accessClassesThat().resideInAPackage("..runtime..")
-				.check(importedClasses);
-		noClasses().that().resideInAPackage("..spi..")
 				.should().accessClassesThat().resideInAPackage("..runtime..")
 				.check(importedClasses);
 	}
