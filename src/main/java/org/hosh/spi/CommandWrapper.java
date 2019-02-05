ddf71a9fff1b1c50f29f2fe3b07926d7908b3b3b
Davide Angelocola <davide.angelocola@gmail.com>
2019-02-05 09:39:42 +0100

    spi: more honest api

--- src/main/java/org/hosh/spi/CommandWrapper.java
+++ src/main/java/org/hosh/spi/CommandWrapper.java
@@ -2,15 +2,12 @@ package org.hosh.spi;
 
 import java.util.List;
 
+import org.hosh.doc.Todo;
+
 /**
  * A command specialization that performs set-up and clean-up.
  */
 public interface CommandWrapper<T> extends Command {
-	@Override
-	default ExitStatus run(List<String> args, Channel in, Channel out, Channel err) {
-		return ExitStatus.success();
-	}
-
 	/**
 	 * Create and set-up a resource.
 	 */
@@ -20,4 +17,10 @@ public interface CommandWrapper<T> extends Command {
 	 * Clean-up the resource.
 	 */
 	void after(T resource, Channel in, Channel out, Channel err);
+
+	@Todo(description = "to remove this ugliness it is required to provide a better CommandRegistry")
+	@Override
+	default ExitStatus run(List<String> args, Channel in, Channel out, Channel err) {
+		throw new IllegalStateException("should be never called");
+	}
 }
