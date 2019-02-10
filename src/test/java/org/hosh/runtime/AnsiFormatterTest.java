1dae98c81fd344b4f8e5b3d23bca73bea9fa7e9b
Davide Angelocola <davide.angelocola@gmail.com>
2019-02-10 12:13:38 +0100

    missing test

--- src/test/java/org/hosh/runtime/AnsiFormatterTest.java
+++ src/test/java/org/hosh/runtime/AnsiFormatterTest.java
@@ -1,3 +1,26 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017-2018 Davide Angelocola
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.hosh.runtime;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,6 +52,15 @@ public class AnsiFormatterTest {
 		String result = sut.format(logRecord);
 		assertThat(result)
 				.isEqualTo("1970-01-01T00:00:00 [[31mSEVERE[39m] [main] - [31mmessage[39m\norg.hosh.runtime.AnsiFormatterTest$Stacktraceless\n");
+	}
+
+	@Test
+	public void warning() {
+		given(logRecord.getInstant()).willReturn(Instant.EPOCH);
+		given(logRecord.getLevel()).willReturn(Level.WARNING);
+		given(logRecord.getMessage()).willReturn("message");
+		String result = sut.format(logRecord);
+		assertThat(result).isEqualTo("1970-01-01T00:00:00 [[33mWARNING[39m] [main] - [33mmessage[39m\n");
 	}
 
 	@Test
