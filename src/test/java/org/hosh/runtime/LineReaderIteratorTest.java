7bf8894c0348587e863d7cf3ec6a11ffb21e58ad
Davide Angelocola <davide.angelocola@gmail.com>
2019-03-14 20:35:10 +0100

    wrong years in LICENSE

--- src/test/java/org/hosh/runtime/LineReaderIteratorTest.java
+++ src/test/java/org/hosh/runtime/LineReaderIteratorTest.java
@@ -1,7 +1,7 @@
 /**
  * MIT License
  *
- * Copyright (c) 2017-2018 Davide Angelocola
+ * Copyright (c) 2018-2019 Davide Angelocola
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
