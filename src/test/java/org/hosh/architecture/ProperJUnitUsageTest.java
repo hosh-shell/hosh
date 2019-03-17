be1286b6243df3af8c9c4e9ae22883129586607b
Davide Angelocola <davide.angelocola@gmail.com>
2019-03-17 17:01:14 +0100

    upgrade test dependency FastClasspathScanner -> ClassGraph

--- src/test/java/org/hosh/architecture/ProperJUnitUsageTest.java
+++ src/test/java/org/hosh/architecture/ProperJUnitUsageTest.java
@@ -32,34 +32,27 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ScanResult;
 
 public class ProperJUnitUsageTest {
 	@Test
 	public void enforcePresenceOfTestAnnotation() {
-		new FastClasspathScanner("-jar:")
-				.scan()
-				.getNamesOfAllClasses()
-				.stream()
-				.filter(n -> n.startsWith("org.hosh"))
-				.filter(n -> n.endsWith("Test") || n.endsWith("IT"))
-				.map(this::load)
-				.flatMap(c -> Stream.of(c.getDeclaredMethods()))
-				.filter(m -> Modifier.isPublic(m.getModifiers()))
-				.filter(m -> !Modifier.isStatic(m.getModifiers()))
-				.filter(m -> !m.isAnnotationPresent(Before.class))
-				.filter(m -> !m.isAnnotationPresent(After.class))
-				.filter(m -> !m.isAnnotationPresent(Test.class))
-				.forEach(m -> {
-					fail("method should be annotated with @Test: " + m);
-				});
-	}
-
-	private Class<?> load(String name) {
-		try {
-			return Class.forName(name);
-		} catch (ReflectiveOperationException e) {
-			throw new IllegalArgumentException(e);
+		try (ScanResult scanResult = new ClassGraph().whitelistPackages("org.hosh").scan()) {
+			scanResult
+					.getAllClasses()
+					.loadClasses()
+					.stream()
+					.filter(c -> c.getName().endsWith("Test") || c.getName().endsWith("IT"))
+					.flatMap(c -> Stream.of(c.getDeclaredMethods()))
+					.filter(m -> Modifier.isPublic(m.getModifiers()))
+					.filter(m -> !Modifier.isStatic(m.getModifiers()))
+					.filter(m -> !m.isAnnotationPresent(Before.class))
+					.filter(m -> !m.isAnnotationPresent(After.class))
+					.filter(m -> !m.isAnnotationPresent(Test.class))
+					.forEach(m -> {
+						fail("method should be annotated with @Test: " + m);
+					});
 		}
 	}
 }
