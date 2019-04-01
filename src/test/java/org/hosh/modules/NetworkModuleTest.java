573e7545ee04f835a89df50560fddf440a775843
Davide Angelocola <davide.angelocola@gmail.com>
2019-04-01 08:45:52 +0200

    test coverage for 'network'

---
+++ src/test/java/org/hosh/modules/NetworkModuleTest.java
@@ -0,0 +1,87 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018-2019 Davide Angelocola
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.hosh.modules;
+
+import static org.hosh.testsupport.ExitStatusAssert.assertThat;
+import static org.mockito.BDDMockito.then;
+
+import java.util.Arrays;
+
+import org.hosh.doc.Todo;
+import org.hosh.modules.NetworkModule.Network;
+import org.hosh.spi.Channel;
+import org.hosh.spi.ExitStatus;
+import org.hosh.spi.Keys;
+import org.hosh.spi.Record;
+import org.hosh.spi.Values;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+		NetworkModuleTest.NetworkTest.class,
+})
+public class NetworkModuleTest {
+
+	@RunWith(MockitoJUnitRunner.StrictStubs.class)
+	public static class NetworkTest {
+
+		@Mock
+		private Channel in;
+
+		@Mock
+		private Channel out;
+
+		@Mock
+		private Channel err;
+
+		@InjectMocks
+		private Network sut;
+
+		@Todo(description = "this is a very bland test, let's try to consolidate this command before investing more")
+		@Test
+		public void noArgs() {
+			ExitStatus exitStatus = sut.run(Arrays.asList(), in, out, err);
+			assertThat(exitStatus).isSuccess();
+			then(in).shouldHaveZeroInteractions();
+			then(out).should(Mockito.atLeastOnce()).send(Mockito.any(Record.class));
+			then(err).shouldHaveZeroInteractions();
+		}
+
+		@Test
+		public void oneArg() {
+			ExitStatus exitStatus = sut.run(Arrays.asList("whatever"), in, out, err);
+			assertThat(exitStatus).isError();
+			then(in).shouldHaveZeroInteractions();
+			then(out).shouldHaveZeroInteractions();
+			then(err).should().send(Record.of(Keys.ERROR, Values.ofText("expected 0 arguments")));
+		}
+	}
+}
