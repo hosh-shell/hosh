04c256ebc1e725151ce98c698dce4d88f26f3d97
Davide Angelocola <davide.angelocola@gmail.com>
2019-04-21 00:07:58 +0200

    Revert "tentative fix errors"
    This reverts commit 6173ff2e180e670cd561501a8087c9cfa85c3cec.

--- src/test/java/org/hosh/runtime/CommandResolversTest.java
+++ src/test/java/org/hosh/runtime/CommandResolversTest.java
@@ -116,10 +116,7 @@ public class CommandResolversTest {
 
 		@Test
 		public void foundNonExecutableInPath() throws IOException {
-			File newFile = folder.newFile("test");
-			newFile.setExecutable(true);
-			newFile.setExecutable(false);
-			assert !newFile.canExecute();
+			folder.newFile("test");
 			given(state.getCommands()).willReturn(Collections.emptyMap());
 			given(state.getPath()).willReturn(Arrays.asList(folder.getRoot().toPath().toAbsolutePath()));
 			given(state.getCwd()).willReturn(Paths.get("."));
