17ba8243736d10dbc8407babed51d94d9f983ac6
Davide Angelocola <davide.angelocola@gmail.com>
2019-05-24 08:16:27 +0200

    fix log call

--- src/main/java/org/hosh/Hosh.java
+++ src/main/java/org/hosh/Hosh.java
@@ -181,7 +181,7 @@ public class Hosh {
 					return exitStatus;
 				}
 			} catch (Exception e) {
-				logger.log(Level.SEVERE, String.format("caught exception for input: '%s'", line), e);
+				logger.log(Level.SEVERE, String.format("caught exception for input: '%s'", line.get()), e);
 				err.send(Records.singleton(Keys.ERROR, Values.ofText(Objects.toString(e.getMessage(), "(no message)"))));
 			}
 		}
